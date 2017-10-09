9b2dfb4e72ab465c55332a426184e9d616732373
Roman Safronov <r.safronik@gmail.com>
2017-10-09 15:30:11 +0500

    Update config.php

--- cleantalk/config.php
+++ cleantalk/config.php
@@ -1,32 +1,5 @@
 <?php
 	
-	//Auth key
-	$auth_key = 'mame9ajavehy';
-	
-	//Auth key
-	$auth_key = 'mame9ajavehy';
-	
-	//Auth key
-	$auth_key = 'mame9ajavehy';
-	
-	//Auth key
-	$auth_key = 'mame9ajavehy';
-	
-	//Auth key
-	$auth_key = 'mame9ajavehy';
-	
-	//Auth key
-	$auth_key = 'mame9ajavehy';
-	
-	//Auth key
-	$auth_key = 'mame9ajavehy';
-	
-	//Auth key
-	$auth_key = 'mame9ajavehy';
-	
-	//Auth key
-	$auth_key = 'mame9ajavehy';
-		
 	// Response language
 	$response_lang = 'en';
 	
