5cc212c730548d57ae3513080ecdbcded23aef2e
davydov <artem@ip-192-168-0-101.is74.loc>
2019-02-18 10:38:39 +0500

    fix jquery link

--- ctsetup.php
+++ ctsetup.php
@@ -186,7 +186,7 @@ define('DS', DIRECTORY_SEPARATOR);
      		<p id="footer-text"><small>It is highly recommended to create a backup before installation</small></p>
      	</footer>
 
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="cleantalk/js/jquery-1.9.1.min.js"><\/script>')</script> 
         <script src="cleantalk/js/bootstrap.min.js"></script> 
         <script src="cleantalk/js/placeholder-shim.min.js"></script>        
