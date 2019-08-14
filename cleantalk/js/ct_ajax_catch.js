56de1ad287cb847c28f06152b19749c466e9f5ec
Roman Safronov <r.safronik@gmail.com>
2019-08-15 00:20:37 +0500

    AJAX handling
    New: Show message for blocked ajax requests.

---
+++ cleantalk/js/ct_ajax_catch.js
@@ -0,0 +1,12 @@
+// Capturing responses and output block message for unknown AJAX forms
+jQuery(document).ajaxComplete(function(event, xhr, settings) {
+	if(xhr.responseText && xhr.responseText.indexOf('"apbct') !== -1){
+		var response = JSON.parse(xhr.responseText);
+		if(typeof response.apbct !== 'undefined'){
+			var response = response.apbct;
+			if(response.blocked){
+				alert(response.comment);
+			}
+		}
+	}
+});
