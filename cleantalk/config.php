e3ceec9896588710d273ec2f7587e8b9025961b5
safronik <safronov@cleantalk.org>
2019-11-21 14:13:53 +0500

    Update config.php

--- cleantalk/config.php
+++ cleantalk/config.php
@@ -1,16 +1,17 @@
 <?php
-	
-	// Response language
-	$response_lang = 'en';
-	
-	$check_reg = 1;
-	$check_all_post_data = 0;
-	$swf_on = 0;
 
-	$sfw_last_update = 0;
-	$sfw_last_logs_send = 0;
-	
-	$exclusions = array();
-	
-	define('APBCT_VERSION', '2.0');
-	define('APBCT_AGENT', 'uni-20');
+//Settings
+	$registrations_test = true;
+	$general_postdata_test = true;
+	$spam_firewall = true;
+
+// Statistics
+	$sfw_last_update = 1574326874;
+	$sfw_entries = 9809;
+	$sfw_last_logs_send = 1574326113;
+
+// Response language
+$response_lang = 'en';
+
+define('APBCT_VERSION', '2.0');
+define('APBCT_AGENT', 'uni-20');
