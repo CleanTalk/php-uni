74a07acd13589a1ee5d7e6ba6f8d1a472c6f49cb
Viktor <Bazz@bk.ru>
2019-11-29 13:54:57 +0300

    Fix: autoloader fixed.

--- cleantalk/lib/autoloader.php
+++ cleantalk/lib/autoloader.php
@@ -14,6 +14,7 @@ function ct_autoloader( $class ){
 	// Register class auto loader
 	// Custom modules
 	if( strpos( $class, 'Cleantalk' ) !== false ){
+		$class = str_replace( '\\', DIRECTORY_SEPARATOR, $class );
 		$class_file = __DIR__ . DIRECTORY_SEPARATOR . $class . '.php';
 		if( file_exists( $class_file ) ){
 			require_once( $class_file );
