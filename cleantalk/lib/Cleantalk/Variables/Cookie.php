24c40937191a0f211de3cc1ee405b5dc8d591409
Glomberg <bazz@bk.ru>
2022-07-31 13:01:42 +0300

    Upd: Visible fields process logic updated.

--- cleantalk/lib/Cleantalk/Variables/Cookie.php
+++ cleantalk/lib/Cleantalk/Variables/Cookie.php
@@ -13,8 +13,30 @@ namespace Cleantalk\Variables;
 class Cookie extends SuperGlobalVariables{
 	
 	static $instance;
-	
-	/**
+
+    /**
+     * Getting visible fields collection
+     * @return array
+     *
+     * @since version
+     */
+    public static function getVisibleFields()
+    {
+        // Visible fields processing
+        // Get from separated native cookies and convert it to collection
+        $visible_fields_cookies_array = array_filter($_COOKIE, static function ($key) {
+            return strpos($key, 'apbct_visible_fields_') !== false;
+        }, ARRAY_FILTER_USE_KEY);
+        $visible_fields_collection = array();
+        foreach ( $visible_fields_cookies_array as $visible_fields_key => $visible_fields_value ) {
+            $prepared_key = str_replace('apbct_visible_fields_', '', $visible_fields_key);
+            $prepared_value = json_decode(str_replace('\\', '', $visible_fields_value), true);
+            $visible_fields_collection[$prepared_key] = $prepared_value;
+        }
+        return $visible_fields_collection;
+    }
+
+    /**
 	 * Gets given $_COOKIE variable and save it to memory
 	 * @param $name
 	 *
