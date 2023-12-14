9bd990b2d5c5f700a972dc674f888599a88014f5
Glomberg <bazz@bk.ru>
2023-12-14 13:26:15 +0300

    Version: 2.7.1.

--- cleantalk/inc/common.php
+++ cleantalk/inc/common.php
@@ -4,7 +4,7 @@ use Cleantalk\ApbctUni\RemoteCalls;
 use Cleantalk\Variables\Server;
 
 define('APBCT_PLUGIN', 'uni');
-define('APBCT_VERSION', '2.7.0');
+define('APBCT_VERSION', '2.7.1');
 define('APBCT_AGENT', APBCT_PLUGIN . '-' . str_replace( '.', '', APBCT_VERSION ) );
 define('APBCT_USER_AGENT', 'Cleantalk-Antispam-Universal-Plugin/' . APBCT_VERSION);
 define('APBCT_INITIAL_INCLUDE_PATH', get_include_path());
